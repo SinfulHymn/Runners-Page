254c3cf4812833f396bf84d5950f78e157555f53
Alejandro Rojas <kmachappy@pm.me>
2022-04-23 19:57:24 -0700

    added head to head partials

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 // imports
 require("dotenv").config()
 const express = require("express");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan")
-const PORT = process.env.PORT
-const dbURL = process.env.MONGO_URL
+
 const eventRouter = require("./controllers/events")
 
 // express app
@@ -16,7 +15,7 @@ const app = express();
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
 app.use(methodOverride("_method"))
-app.use(morgan("tiny"))
+app.use(morgan("dev"))
 
 // routes
 app.use(eventRouter)
@@ -24,5 +23,5 @@ app.use(eventRouter)
 
 
 // listener
-
+const PORT = process.env.PORT
 app.listen(PORT, ()=> console.log(`We are listening on ${PORT}`)) 
